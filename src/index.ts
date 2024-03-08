ccb2e4ae463ae1bbd3a4c93e2ca7a176a8cc49c5
hollowww-dev <me@hollowww.dev>
2024-03-09 00:53:19 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/api/ping', (_req, res) => {
+	console.log('someone pinged here :)');
+	res.send('pong');
+});
+
+app.listen(PORT, () => {
+	console.log(`server is running on port ${PORT}`);
+});
